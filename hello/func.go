59cbc90d82d96d8be62cc1ad1e7611784bafe9b8
hiroyuki_sakurada <sanno.phill@gmail.com>
2021-04-09 10:50:11 +0900

    add some func

--- hello/func.go
+++ hello/func.go
@@ -6,6 +6,21 @@ func add(x, y int) int {
 	return x * y
 }
 
+// 複数戻り値を返す事が可能
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+// 戻り値に名前をつける事が可能
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
 func main() {
 	fmt.Println(add(42, 13))
+	a, b := swap("hello", "Go")
+	fmt.Println(a, b)
+	fmt.Println(split(17))
 }
