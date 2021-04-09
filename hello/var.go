845d5ea1ddb9f6434565aaba459c63d2a31291b9
hiroyuki_sakurada <sanno.phill@gmail.com>
2021-04-09 11:07:23 +0900

    create var.go

---
+++ hello/var.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var c, python, java bool
+
+// 初期化
+var ruby, php, swift = true, false, "for apple app"
+
+func main() {
+	var i int
+	fmt.Println(i, c, python, java)
+	fmt.Println(ruby, php, swift)
+
+	kotlin := "made in Russia"
+	fmt.Println(kotlin)
+}
